9aaef0296fdc661d83d875f6cf6d2ae63954045e
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-07-06 18:39:44 -0500

    error include urls.py de travello

--- destinosTuristicos/urls.py
+++ destinosTuristicos/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from travello import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 # funciona cuerpo de index
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", include('travello.urls')), #views.index, name="index"
     path('admin/', admin.site.urls),
 ]
 
